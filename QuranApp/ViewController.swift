44b963148694aa2c24a36e30f3087da7140292a0
Xursandbek <xursandbek06082001@mail.com>
2023-06-24 11:48:36 +0500

    Initial Commit

---
+++ QuranApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuranApp
+//
+//  Created by Xursandbek Qambaraliyev on 24/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
